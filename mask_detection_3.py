3768c0b65faf17cef65304b893a82cbca010f522
Luis Campero <144020199+lcr29@users.noreply.github.com>
2024-03-05 20:27:03 +0100

    mask_detection_3.py

--- mask_detection_3.py
+++ mask_detection_3.py
@@ -36,6 +36,9 @@ count_people_loop = 0
 minute_start_time = time.time()
 minute_count = 0
 
+# Define the path to your alarm sound file
+alarm_sound_path = "MV27TES-alarm.wav"
+
 # Initialize Pygame
 pygame.init()
 
